fa34f125945d95b7f17918667021ddb918c2e43a
Egor <egorstarostenko491@gmail.com>
2025-12-05 17:20:31 +0200

    Short clear message

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,11 +1,88 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
+import I18nContext from "../contexts/I18nContext";
 
+const initialValues = {
+  fullname: "",
+  age: "",
+  password: "",
+  repeatpassword: "",
+};
+
+const validationSchema = Yup.object().shape({
+  fullname: Yup.string()
+    .min(2, "Name must be more than 2")
+    .max(18, "Name must be less 18")
+    .required("Name is required"),
+  age: Yup.number()
+    .typeError("Age must be a number")
+    .positive("Age must be positive")
+    .integer("Age must be an integer"),
+  password: Yup.string()
+    .matches(
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+      "Password incorrect"
+    )
+    .required("Password is required"),
+
+  repeatpassword: Yup.string()
+    .oneOf([Yup.ref("password")], "Passwords must match")
+    .required("Repeat password"),
+});
 const Registration = () => {
-    return (
-      <div>
-        <h1>Registration page</h1>
-      </div>
-    );
-}
+  const { currentTexts } = useContext(I18nContext);
+
+  const submitHandler = (values, { resetForm }) => {
+    console.log(values);
+    resetForm();
+  };
+
+  return (
+    <div>
+      <h1>{currentTexts.titleRegistration}</h1>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={submitHandler}
+        validationSchema={validationSchema}
+      >
+        {() => (
+          <Form>
+            <div>
+              <Field name="fullname" placeholder={currentTexts.name} />
+              <ErrorMessage name="fullname" component="div" className="error" />
+            </div>
+            <div>
+              <Field name="age" type="number" placeholder={currentTexts.age} />
+              <ErrorMessage name="age" component="div" className="error" />
+            </div>
+            <div>
+              <Field
+                type="password"
+                name="password"
+                placeholder={currentTexts.password}
+              />
+              <ErrorMessage name="password" component="div" className="error" />
+            </div>
+            <div>
+              <Field
+                type="password"
+                name="repeatpassword"
+                placeholder={currentTexts.repeatpassword}
+              />
+              <ErrorMessage
+                name="repeatpassword"
+                component="div"
+                className="error"
+              />
+            </div>
+
+            <button type="submit">{currentTexts.button}</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
 
 export default Registration;
--- src/translations/translations.js
+++ src/translations/translations.js
@@ -2,16 +2,28 @@ const translations = {
   en: {
     titleHome: "Home page",
     titleBooking: "Booking page",
+    titleRegistration: "Registratiton page",
+    name: "Full name",
+    age: "Age",
+    password: "Enter password",
+    repeatpassword: "Repeat password",
     aboutHome:
       "Our service helps you easily plan and book your trips.Choose travel dates, select the number of people, manage bookings, and enjoy a simple and convenient interface with language and theme customization.",
     booknowHome: "Book now",
+    button: "Submit",
   },
   ua: {
     titleHome: "Домашня сторінка",
     titleBooking: "Сторінка бронювання",
+    titleRegistration: "Сторінка реєстрації",
+    name: "Повне ім'я",
+    age: "Вік",
+    password: "Уведіть пароль",
+    repeatpassword: "Повторіть пароль",
     aboutHome:
       "Наш сервіс допомагає легко планувати та бронювати подорожі.Обирайте дати поїздки, кількість людей, керуйте бронюваннями та користуйтеся простим і зручним інтерфейсом з підтримкою мови та теми.",
     booknowHome: "Забронювати зараз",
+    button: "Прийняти",
   },
 };
 export default translations;
